5f33d335a746125012b1e0a2e45faf67bf62e3c0
Ayush201738982361 <ayushnair87@gmail.com>
2025-05-20 14:44:36 +0530

    Function for rendering contact section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,34 @@
 import Navbar from "./components/Navbar";
 import AboutMe from "./components/AboutMe";
 import EducationTimeline from "./components/EducationTimeline";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Form from "./components/Form";
+import { useState, useRef } from "react";
 import "./App.css";
 
 function App() {
+  const [section, renderSection] = useState(false);
+  const formRef = useRef(null);
+
+  const toggleSection = () => {
+    renderSection((prev) => !prev);
+
+    if (!section && formRef.current) {
+      formRef.current.scrollIntoView({ behavior: "auto" });
+    }
+  };
+
   return (
     <>
-      <Navbar />
+      <Navbar toggleSection={toggleSection} />
       <AboutMe />
       <EducationTimeline />
+      <Skills />
+      <Projects />
+      <div ref={formRef}>
+        <Form></Form>
+      </div>
     </>
   );
 }
