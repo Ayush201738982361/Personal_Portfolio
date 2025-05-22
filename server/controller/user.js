60f4db883e8e6c971e68767579622d08797d758b
Ayush201738982361 <ayushnair87@gmail.com>
2025-05-23 00:39:20 +0530

    Sent status code 409 if the email exists in db

--- server/controller/user.js
+++ server/controller/user.js
@@ -2,10 +2,21 @@ const message = require("../model/user");
 
 async function createUserMsg(req, res) {
   try {
+    const { email } = req.body;
+    const existingEmail = await message.find({ email });
+
+    if (existingEmail.length > 0) {
+      return res.status(409).json({ message: "Email Already Exists" });
+    }
     const newMessage = await message.create(req.body);
     console.log(newMessage);
     res.status(201).json({ stat: "User message send" });
   } catch (err) {
+    if (err.code === 11000) {
+      return res
+        .status(409)
+        .json({ message: "Email already exists (DB constraint)" });
+    }
     console.log("Error in creating user", err);
   }
 }
