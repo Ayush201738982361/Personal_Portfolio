77180d47a5f4e2beba7705cc62ec259c93b619f3
Ayush201738982361 <ayushnair87@gmail.com>
2025-05-28 12:54:24 +0530

    Removed image and added new hero text

--- client/src/components/AboutMe.jsx
+++ client/src/components/AboutMe.jsx
@@ -1,24 +1,14 @@
-import aboutme from "../public/about.png";
+import "../App.css";
 
 function AboutMe() {
+  const name = "Ayush S Nair";
+  const profession = "Software Developer";
+
   return (
-    <>
-      <div className="about-container">
-        <div className="text-section">
-          <p>
-            Hi there , My name is Ayush S Nair ! I'm a passionate Computer
-            Science student with a keen interest in creating dynamic and
-            responsive web applications. My expertise lies in using the MERN
-            stack (MongoDB, Express.js, React.js, and Node.js), which allows me
-            to build full-stack applications that are both powerful and
-            scalable.
-          </p>
-        </div>
-        <div className="image-section">
-          <img src={aboutme} alt="about" />
-        </div>
-      </div>
-    </>
+    <div className="hero-container">
+      <h1>I'm {name}</h1>
+      <h2 className="profession">{profession}</h2>
+    </div>
   );
 }
 
