1df2b5e9346a79b7576a6dd763867e1f098ed401
Ayush201738982361 <ayushnair87@gmail.com>
2025-05-29 18:31:55 +0530

    Refactored Skill component

---
+++ client/src/components/Skill.jsx
@@ -0,0 +1,99 @@
+function Skill() {
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "50px",
+          marginBottom: "40px",
+        }}
+      >
+        <h3 className="section-heading">Skills</h3>
+      </div>
+      <div className="skill-container">
+        <img
+          style={{ height: "100px" }}
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original.svg"
+        />
+        <img
+          style={{ height: "100px" }}
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongoose/mongoose-original.svg"
+        />
+        <img
+          style={{ height: "100px" }}
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"
+        />
+        <img
+          style={{ height: "100px" }}
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg"
+        />
+
+        <img
+          style={{ height: "100px" }}
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-plain-wordmark.svg"
+        />
+        <img
+          style={{ height: "100px" }}
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodemon/nodemon-line.svg"
+        />
+        <img
+          style={{
+            backgroundColor: "white",
+            height: "90px",
+            borderRadius: "50px",
+          }}
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/express/express-original-wordmark.svg"
+        />
+
+        <img
+          style={{ height: "100px" }}
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bootstrap/bootstrap-original.svg"
+        />
+
+        <img
+          style={{ height: "100px" }}
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-plain-wordmark.svg"
+        />
+        <img
+          style={{ height: "100px" }}
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-plain-wordmark.svg"
+        />
+        <img
+          style={{ height: "100px", borderRadius: "10px" }}
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg"
+        />
+        <img
+          style={{ height: "100px" }}
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cplusplus/cplusplus-original.svg"
+        />
+        <img
+          style={{ height: "100px" }}
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original-wordmark.svg"
+        />
+        <img
+          style={{ height: "100px" }}
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original-wordmark.svg"
+        />
+        <img
+          style={{ height: "100px" }}
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original-wordmark.svg"
+        />
+        <img
+          style={{ height: "100px" }}
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original-wordmark.svg"
+        />
+        <img
+          style={{ height: "100px" }}
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/linux/linux-original.svg"
+        />
+        <img
+          style={{ height: "100px" }}
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postman/postman-original.svg"
+        />
+      </div>
+    </>
+  );
+}
+
+export default Skill;
